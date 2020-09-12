b4976675f788bc6cc3578fedf33cc98cf9086975
SamKlep <shamdemann23@gmail.com>
2020-09-12 10:37:19 -0500

    Implemented third-party api and rendered data on to home.

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 // Stock Market Portfolio App
-
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const request = require("request");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// API KEY pk_145065d7af7646a0961e795aa8d0b395
+// Create call_api function
+function call_api(finishedAPI) {
+  request(
+    "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_145065d7af7646a0961e795aa8d0b395",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (res.statusCode === 200) {
+        // console.log(body);
+        finishedAPI(body);
+      }
+    }
+  );
+}
 
 // Set Handlebars Middleware
 app.engine("handlebars", exphbs());
@@ -16,9 +35,16 @@ const otherstuff = "This is other stuff";
 
 // Set handlebar routes
 app.get("/", function (req, res) {
-  res.render("home", {
-    stuff: otherstuff,
+  call_api(function (doneAPI) {
+    res.render("home", {
+      stock: doneAPI,
+    });
   });
+});
+
+// Create about page routes
+app.get("/about", function (req, res) {
+  res.render("about", {});
 });
 
 // Set Static Folder
